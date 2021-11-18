614ce859f8741a2f6246594d24e56fb44ee99994
Cesar Garcia <cesar.garcia@ceos.com.ve>
2021-11-17 21:56:36 -0400

    Init HA feature.

--- plc4j/drivers/s7/src/main/java/org/apache/plc4x/java/s7/readwrite/S7HADriver.java
+++ plc4j/drivers/s7/src/main/java/org/apache/plc4x/java/s7/readwrite/S7HADriver.java
@@ -1,13 +1,51 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.plc4x.java.s7.readwrite;
+
+import org.apache.plc4x.java.api.PlcConnection;
+import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
+import org.apache.plc4x.java.s7.readwrite.configuration.S7Configuration;
+import org.apache.plc4x.java.spi.configuration.Configuration;
 
 /**
  *
  * @author cgarcia
  */
-public class S7HADriver {
+public class S7HADriver extends S7Driver {
+
+    
+    @Override
+    public String getProtocolCode() {
+        return "s7h";
+    }    
+    
+    @Override
+    public String getProtocolName() {
+        return "Siemens S7 (High Availability)";
+    }
+    
+    @Override
+    public PlcConnection getConnection(String connectionString) throws PlcConnectionException {
+        System.out.println("La conexion: " + connectionString);
+        return super.getConnection(connectionString); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
     
 }
